f6c80b9c138bb33072ca70d1f9dcecbed4ff95ea
Sammybett-bit <bettsammy20@gmail.com>
2023-11-24 13:22:48 +0300

    Implemented asset seeding of 50 assets

--- app/seed.py
+++ app/seed.py
@@ -14,3 +14,23 @@ with db.app.app_context():
     Requests.query.delete()
 
     print('Deleting existing data from databases')
+
+    
+    assets =[]
+
+    for i in range(50):
+        asset = Asset(
+            assetName=fake.word(),
+            model=fake.word(),
+            imageUrl=fake.image_url(),  # Use Faker's image_url to generate image URLs
+            manufacturer=fake.word(),
+            datePurchased=fake.date_time(),
+            status=rc(['Active', 'Pending', 'Under Maintenance']),
+            category=fake.word()
+        )
+
+        assets.append(asset)
+        db.session.add_all(assets)
+        db.session.commit()
+
+    print('Generating assets')
