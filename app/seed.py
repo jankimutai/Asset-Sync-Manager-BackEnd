dad808b1594db2a27f9e5b5cc439609099bff145
Sammybett-bit <bettsammy20@gmail.com>
2023-11-24 13:08:34 +0300

    Implemented database reset

--- app/seed.py
+++ app/seed.py
@@ -4,3 +4,13 @@ from faker import Faker
 from random import choice as rc
 
 fake = Faker()
+
+with db.app.app_context():
+    Asset.query.delete()
+    User.query.delete()
+    Assignment.query.delete()
+    Maintenance.query.delete()
+    Transaction.query.delete()
+    Requests.query.delete()
+
+    print('Deleting existing data from databases')
