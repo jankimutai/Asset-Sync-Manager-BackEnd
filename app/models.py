4fdd5a10631e70dc2cd37fc39e5a4bf43aea3ba6
Kimutai <jankimsingoei@gmail.com>
2023-12-15 16:00:37 +0300

    Bug fix 2

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from config import db, bcrypt
+from app.config import db, bcrypt
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
